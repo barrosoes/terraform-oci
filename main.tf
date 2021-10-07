58cfcb1345f73d82b94bf9f36955b0510028e2e4
barrosoes <barrosoes@gmail.com>
2021-10-07 12:51:45 -0300

    Mudando 2

--- main.tf
+++ main.tf
@@ -216,6 +216,10 @@ resource "oci_load_balancer" "lb1" {
   is_private                 = true
  # network_security_group_ids = [oci_core_network_security_group.test_network_security_group.id]
 
+shape_details {
+      maximun_bandwidth_in_mbps = var.load_balancer_max_band
+      minimun_bandwidth_in_mbps = var.load_balancer_min_band
+    }
 }
 
 resource "oci_load_balancer_backend_set" "lb-bes1" {
