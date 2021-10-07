22379b85fb4e9b1a0f833af58963003cfa4f7f99
barrosoes <barrosoes@gmail.com>
2021-10-07 01:13:07 -0300

    cria lb1

--- main.tf
+++ main.tf
@@ -208,7 +208,7 @@ resource "oci_load_balancer_listener" "lb-listener1" {
 
 resource "oci_load_balancer_backend" "lb-be1" {
   load_balancer_id = oci_load_balancer.lb1.id
-  backendset_name  = oci_load_balancer_backend_set.lb-bes1.name
+  backendset_name  = oci_load_balancer_backend_set.webserver1.name
   ip_address       = oci_core_instance.webserver1.private_ip
   port             = 80
   backup           = false
